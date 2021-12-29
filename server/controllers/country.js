4b51acf7c2e88934ed53e605b3a0ddd2d6d89628
Paras Jain <parasmani.jain2208@gmail.com>
2021-12-29 21:24:09 +0530

    replacing populate with aggregate

--- server/controllers/country.js
+++ server/controllers/country.js
@@ -1,22 +1,59 @@
-const { Country, City, Ground } = require('../models/schemaModel');
+const { Country } = require('../models/schemaModel');
 
 const getCountryList = (req, res) => {
-    // get data from the view and add it to mongodb
-    Country.find({}, null, { sort: { name: 1 } })
-        .exec(function (err, results) {
+    Country.aggregate(
+        [
+            {
+                "$project": {
+                    "name": 1
+                }
+            },
+            { "$sort": { "name": 1 } }
+        ],
+        function (err, results) {
             if (err) return res.send(500, { error: err });
             return res.send(results);
-        });
+        })
 };
 
 const getCountryCityList = (req, res) => {
-    // get data from the view and add it to mongodb
-    Country.find({}, null, { sort: { name: 1 } })
-        .populate({ path: 'cities', options: { sort: { 'name': 1 } } })
-        .exec(function (err, results) {
+    Country.aggregate(
+        [
+            {
+                "$lookup": {
+                    "from": 'cities',
+                    "let": { "cities": "$cities" },
+                    "as": 'cities',
+                    "pipeline": [
+                        {
+                            "$match": { "$expr": { "$in": ["$_id", "$$cities"] } }
+
+                        },
+                        { "$sort": { "name": 1 } }
+
+                    ]
+                }
+            }, {
+                "$project": {
+                    "name": 1,
+                    "cities": {
+                        "$map": {
+                            "input": "$cities",
+                            "as": "c",
+                            "in": {
+                                "name": "$$c.name",
+                                "_id": "$$c._id"
+                            }
+                        }
+                    }
+                }
+            },
+            { "$sort": { "name": 1 } }
+        ],
+        function (err, results) {
             if (err) return res.send(500, { error: err });
             return res.send(results);
-        });
+        })
 };
 
 const getCountryGroundList = (req, res) => {
@@ -34,41 +71,42 @@ const getCountryGroundCount = (req, res) => {
     Country.aggregate(
         [
             {
-            "$lookup": {
-                "from": "cities",
-                "let": { "cities": "$cities" },
-                "pipeline": [
-                    { "$match": { "$expr": { "$in": ["$_id", "$$cities"] } } },
-                    {
-                        "$lookup": {
-                            "from": "grounds",
-                            "let": { "grounds": "$grounds" },
-                            "pipeline": [
-                                { "$match": { "$expr": { "$in": ["$_id", "$$grounds"] } } },
-                            ],
-                            "as": "grounds"
+                "$lookup": {
+                    "from": "cities",
+                    "let": { "cities": "$cities" },
+                    "pipeline": [
+                        { "$match": { "$expr": { "$in": ["$_id", "$$cities"] } } },
+                        {
+                            "$lookup": {
+                                "from": "grounds",
+                                "let": { "grounds": "$grounds" },
+                                "pipeline": [
+                                    { "$match": { "$expr": { "$in": ["$_id", "$$grounds"] } } },
+                                ],
+                                "as": "grounds"
+                            }
                         }
-                    }
-                ],
-                "as": "cities"
-            }
-        },
-          {
-            "$project": {
-                "name": 1,
-                "city": {
-                    "$map": {
-                        "input": "$cities",
-                        "as": "c",
-                        "in": {
-                            "ground": {
-                                "$map": {
-                                    "input": "$$c.grounds",
-                                    "as": "g",
-                                    "in": {
-                                        "name": "$$g.name",
-                                        "length": {
-                                            "$size": "$$g.matches"
+                    ],
+                    "as": "cities"
+                }
+            },
+            {
+                "$project": {
+                    "name": 1,
+                    "city": {
+                        "$map": {
+                            "input": "$cities",
+                            "as": "c",
+                            "in": {
+                                "ground": {
+                                    "$map": {
+                                        "input": "$$c.grounds",
+                                        "as": "g",
+                                        "in": {
+                                            "name": "$$g.name",
+                                            "length": {
+                                                "$size": "$$g.matches"
+                                            }
                                         }
                                     }
                                 }
@@ -76,9 +114,8 @@ const getCountryGroundCount = (req, res) => {
                         }
                     }
                 }
-            }
-        },
-        { "$sort": { "name": 1 } },
+            },
+            { "$sort": { "name": 1 } },
         ],
         function (err, results) {
             if (err) return res.send(500, { error: err });
